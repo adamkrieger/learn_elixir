ae419fbad7c91af10cb6ff40734755d0a778d68d
adamkrieger <adamkrieger.ca@gmail.com>
2015-07-04 13:25:24 -0500

    Chop: Made Chop.mid private and added notation for uncared-for bindings.

--- chop.exs
+++ chop.exs
@@ -9,23 +9,23 @@ defmodule Chop do
     guess(answer, mid(lower..upper), lower..upper)
   end
 
-  def guess(answer, guess, lower..upper) when guess == answer do
+  def guess(answer, guess, _lower.._upper) when guess == answer do
     IO.puts "It is #{to_string(guess)}!!!"
   end
 
-  def guess(answer, guess, lower..upper) when guess < answer do
+  def guess(answer, guess, _lower..upper) when guess < answer do
     IO.puts "Greater than #{to_string(guess)}"
     new_guess = mid(guess..upper)
     guess(answer, new_guess, guess..upper)
   end
 
-  def guess(answer, guess, lower..upper) when guess > answer do
+  def guess(answer, guess, lower.._upper) when guess > answer do
     IO.puts "Less than #{to_string(guess)}"
     new_guess = mid(lower..guess)
     guess(answer, new_guess, lower..guess)
   end
 
-  def mid(lower..upper) do
+  defp mid(lower..upper) do
     lower + div((upper-lower), 2)
   end
 end
