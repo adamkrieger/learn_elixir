9262d325c0b271cad53c50469cd4f7ccc74a2f97
adamkrieger <adamkrieger.ca@gmail.com>
2015-07-06 22:41:09 -0500

    Quicksort: More optimized version of an immutable sort.

--- quicksort.exs
+++ quicksort.exs
@@ -7,9 +7,31 @@ defmodule Quicksort do
   def run(list = [head | _]), do: _run(list, head)
 
   defp _run([pivot | tail], pivot) do
-    left = Enum.filter(tail, &(&1 < pivot))
-    right = Enum.filter(tail, &(&1 > pivot))
-    match = Enum.filter(tail, &(&1 == pivot))
-    List.flatten([run(left), pivot, match, run(right)])
+    {lt, eq, gt} = _sort(tail, pivot)
+    List.flatten([run(lt), pivot, eq, run(gt)])
+  end
+
+  defp _sort([], _) do
+    {[],[],[]}
+  end
+  defp _sort(list, pivot) do
+    _sort({[],[],[]}, list, pivot)
+  end
+
+  defp _sort({lt, eq, gt}, [], _) do
+    IO.puts "break"
+    {List.flatten(lt), eq, List.flatten(gt)}
+  end
+  defp _sort({lt, eq, gt}, [pivot | tail], pivot) do
+    IO.puts "eq"
+    _sort({lt, [eq] ++ [pivot], gt}, tail, pivot)
+  end
+  defp _sort({lt, eq, gt}, [head | tail], pivot) when head < pivot do
+    IO.puts "lt"
+    _sort({[lt] ++ [head], eq, gt}, tail, pivot)
+  end
+  defp _sort({lt, eq, gt}, [head | tail], pivot) when head > pivot do
+    IO.puts "gt"
+    _sort({lt, eq, [gt] ++ [head]}, tail, pivot)
   end
 end
