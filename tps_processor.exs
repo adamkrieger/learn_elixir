a52da3885037f9743fbc09b9c6f4c4ae4e035fdd
adamkrieger <adamkrieger.ca@gmail.com>
2015-07-28 12:29:58 -0500

    Fixed a typo in the processing method names.

--- tps_processor.exs
+++ tps_processor.exs
@@ -7,17 +7,17 @@ defmodule TpsProcessor do
   Did you get that memo? Ref: 3
   :ok
   """
-  
+
   def process(tpsReports) do
     covered_uncovered = tpsReports
     |> Enum.sort(&(_sort_covered_reports(&1, &2)))
     |> Enum.split_while(&(&1.hasCover))
 
     elem(covered_uncovered, 0)
-    |> Enum.each(&(tps_report_filer(&1)))
+    |> Enum.each(&(_tps_report_filer(&1)))
 
     elem(covered_uncovered, 1)
-    |> Enum.each(&(cover_sheet_reminder(&1)))
+    |> Enum.each(&(_cover_sheet_reminder(&1)))
   end
 
   defp _sort_covered_reports(%{:hasCover => false}, %{:hasCover => true}) do
