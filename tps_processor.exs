e1f7d0ccbeb98b4f7fb9e89eab2fa7634fe36a89
adamkrieger <adamkrieger.ca@gmail.com>
2015-07-28 12:25:25 -0500

    Added a TpsProcessor module, which sorts a list of maps by one of the keyvalues, splits it into two lists, then processes each list.

---
+++ tps_processor.exs
@@ -0,0 +1,37 @@
+defmodule TpsProcessor do
+  @moduledoc """
+  iex> TpsProcessor.process([%{:id => 1, :hasCover => false}, %{:id => 2, :hasCover => true}, %{:id => 3, :hasCover => false}, %{:id => 4, :hasCover => true}])
+  Excellent work! Ref: 2
+  Excellent work! Ref: 4
+  Did you get that memo? Ref: 1
+  Did you get that memo? Ref: 3
+  :ok
+  """
+  
+  def process(tpsReports) do
+    covered_uncovered = tpsReports
+    |> Enum.sort(&(_sort_covered_reports(&1, &2)))
+    |> Enum.split_while(&(&1.hasCover))
+
+    elem(covered_uncovered, 0)
+    |> Enum.each(&(tps_report_filer(&1)))
+
+    elem(covered_uncovered, 1)
+    |> Enum.each(&(cover_sheet_reminder(&1)))
+  end
+
+  defp _sort_covered_reports(%{:hasCover => false}, %{:hasCover => true}) do
+    false
+  end
+  defp _sort_covered_reports(%{}, %{}) do
+    true
+  end
+
+  defp _cover_sheet_reminder(tpsReport) do
+    IO.puts "Did you get that memo? Ref: #{tpsReport.id}"
+  end
+
+  defp _tps_report_filer(tpsReport) do
+    IO.puts "Excellent work! Ref: #{tpsReport.id}"
+  end
+end
